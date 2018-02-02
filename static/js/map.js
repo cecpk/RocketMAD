db2bdcddefcf30335f486749aceb3e66ca4eff9b
whitewillem <35378709+whitewillem@users.noreply.github.com>
2018-02-02 19:10:20 +0100

    Use google maps app instead of website.

--- static/js/map.js
+++ static/js/map.js
@@ -537,7 +537,7 @@ function getTypeSpan(type) {
 function openMapDirections(lat, lng) { // eslint-disable-line no-unused-vars
     var url = ''
     if (Store.get('mapServiceProvider') === 'googlemaps') {
-        url = 'https://www.google.com/maps/?daddr=' + lat + ',' + lng
+        url = 'http://maps.google.com/maps?q=' + lat + ',' + lng
         window.open(url, '_blank')
     } else if (Store.get('mapServiceProvider') === 'applemaps') {
         url = 'https://maps.apple.com/maps?daddr=' + lat + ',' + lng
