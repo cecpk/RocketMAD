ccb87754e1d8525e50c573781ac4bb60848bb776
clad87 <1458019+clad87@users.noreply.github.com>
2018-02-13 04:47:54 +0100

    Update models.py
    fix AttributeError: 'int' object has no attribute 'pokemon_display'

--- pogom/models.py
+++ pogom/models.py
@@ -2263,7 +2263,7 @@ def parse_map(args, map_dict, scan_coords, scan_location, db_update_queue,
             #Check for costform skin
             if pokemon_id == 351:
                 pokemon[p.encounter_id]['form'] = (p.pokemon_data
-                                                    .pokemon_display.form).pokemon_display.form
+                                                    .pokemon_display.form)
 
             # Updating Pokemon data from PGScout result
             if scout_result and scout_result['success']:
