a23f467eb184777e445f80cf120a356ae62486bd
darkelement1987 <jgernaat1987@gmail.com>
2018-04-08 13:49:11 +0200

    Beat travis

--- static/js/map.js
+++ static/js/map.js
@@ -2965,9 +2965,7 @@ $(function () {
         loadDefaultImages()
     })
 })
-
 $(function () {
-
     moment.locale(language)
     function formatState(state) {
         if (!state.id) {
@@ -2984,11 +2982,9 @@ $(function () {
         )
         return $state
     }
-
     if (Store.get('startAtUserLocation') && getParameterByName('lat') == null && getParameterByName('lon') == null) {
         centerMapOnLocation()
     }
-
     $.getJSON('static/dist/data/moves.min.json').done(function (data) {
         moves = data
     })
@@ -3018,11 +3014,11 @@ $(function () {
                 text: i8ln(value['name']) + ' - #' + key
             })
             if (generateImages) {
-                pokemonIcon = `<img class='pokemon-select-icon' src='${getPokemonRawIconUrl({'pokemon_id':key})}'>`
+                pokemonIcon = `<img class='pokemon-select-icon' src='${getPokemonRawIconUrl({'pokemon_id': key})}'>`
             } else {
                 pokemonIcon = `<i class="pokemon-sprite n${key}"></i>`
             }
-            $('.list').append('<div class=pokemon-icon-sprite data-pkm=' + i8ln(value['name']) + '  data-value=' + key +'><div id=pkid_list>#' + key + '</div>' + pokemonIcon + '<div id=pkname_list>' + i8ln(value['name'])+ '</div></div>')
+            $('.list').append('<div class=pokemon-icon-sprite data-pkm=' + i8ln(value['name']) + ' data-value=' + key + '><div id=pkid_list>#' + key + '</div>' + pokemonIcon + '<div id=pkname_list>' + i8ln(value['name']) + '</div></div>')
             value['name'] = i8ln(value['name'])
             value['rarity'] = i8ln(value['rarity'])
             $.each(value['types'], function (key, pokemonType) {
@@ -3041,45 +3037,45 @@ $(function () {
             templateResult: formatState
         })
 
-        $('.list').on('click', '.pokemon-icon-sprite', function() {
-           var img = $(this)
-           var select = $(this).parent().parent().find('input[id$=pokemon]')
-           var value = select.val().split(',')
-           var id = img.data('value').toString()
-           if (img.hasClass('active')) {
-               select.val(value.filter(function (elem) {
-               return elem !== id
-               }).join(',')).trigger('change')
-               img.removeClass('active')
-           } else {
-               select.val((value.concat(id).join(','))).trigger('change')
-               img.addClass('active')
-           }
+        $('.list').on('click', '.pokemon-icon-sprite', function () {
+            var img = $(this)
+            var select = $(this).parent().parent().find('input[id$=pokemon]')
+            var value = select.val().split(',')
+            var id = img.data('value').toString()
+            if (img.hasClass('active')) {
+                select.val(value.filter(function (elem) {
+                    return elem !== id
+                }).join(',')).trigger('change')
+                img.removeClass('active')
+            } else {
+                select.val((value.concat(id).join(','))).trigger('change')
+                img.addClass('active')
+            }
         })
 
-        $('.search').on('input', function() {
+        $('.search').on('input', function () {
             var searchtext = $(this).val().toString()
             $(this).next('.list').find('.pokemon-icon-sprite').each(function () {
-            if (searchtext === "" ) {
-                $(this).show()
-            } else {
-                if (($(this).data('pkm').toLowerCase().indexOf(searchtext.toLowerCase()) !== -1) || ($(this).data('value').toString() === searchtext.toString())) {
+                if (searchtext === '') {
                     $(this).show()
                 } else {
-                    $(this).hide()
+                    if (($(this).data('pkm').toLowerCase().indexOf(searchtext.toLowerCase()) !== -1) || ($(this).data('value').toString() === searchtext.toString())) {
+                        $(this).show()
+                    } else {
+                        $(this).hide()
+                    }
                 }
-            }
             })
         })
 
 
-             loadDefaultImages()
+        loadDefaultImages()
 
         $('.select-all').on('click', function (e) {
-             e.preventDefault()
-             var parent = $(this).parent()
-             parent.find('.list .pokemon-icon-sprite').addClass('active')
-                    parent.find('input[id$=pokemon]').val(Array.from(Array(numberOfPokemon + 1).keys()).slice(1).join(',')).trigger('change')
+            e.preventDefault()
+            var parent = $(this).parent()
+            parent.find('.list .pokemon-icon-sprite').addClass('active')
+            parent.find('input[id$=pokemon]').val(Array.from(Array(numberOfPokemon + 1).keys()).slice(1).join(',')).trigger('change')
         })
         $('.hide-all').on('click', function (e) {
             e.preventDefault()
