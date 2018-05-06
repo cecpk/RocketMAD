657ada126d722e89feabdb50d2bad266528af93c
cecpk <cec@retrospot.de>
2018-05-06 14:48:55 +0200

    Redesign with Searchcommands, all Presets and Arraycompare function

--- static/js/map.js
+++ static/js/map.js
@@ -24,6 +24,7 @@ var $selectSearchIconMarker
 var $selectLocationIconMarker
 var $switchGymSidebar
 var $selectExcludeRarity
+var pokeSearchList = []
 var pokemonGen = new Array(808)
 pokemonGen.fill(1, 1, 152)
 pokemonGen.fill(2, 152, 252)
@@ -3002,7 +3003,6 @@ $(function () {
     var numberOfPokemon = 384
 
     $('.list').before('<input type="search" class="search" placeholder="Search for Name, ID or Type...">')
-
     const hidepresets  = Store.get('hidepresets')
 
     $.each(hidepresets, function (key, value) {
@@ -3014,56 +3014,38 @@ $(function () {
             pokemonIcon = `<i class="pokemon-sprite n${iconid}"></i>`
         }
 
-    $('.exclude_templates').append('<div class="hidepreset" data-key=' + key + '><div class="hideicon">' + pokemonIcon  + '</div>' + value['Name'] + '</div>') 
+    $('.exclude_templates').append('<div class="hidepreset" data-key=' + key + '><div class="hideicon">' + pokemonIcon  + '</div><div class="hidetext">' + value['Name'] + '</div></div>')
 
     })
-
-    function load_pokemon_json(callback){
-        $.getJSON('static/dist/data/pokemon.min.json', function( data ) {
-           callback(data);
-        })
-    }
 
     // Load pokemon names and populate lists
     $.getJSON('static/dist/data/pokemon.min.json').done(function (data) {
         var pokemonIcon
         var typestring = []
         var pokeList = []
-        var pokeSearchList = []
         $.each(data, function (key, value) {
             if (key > numberOfPokemon) {
                 return false
             }
-            var _types = []
-            pokeList.push({
-                id: key,
-                text: i8ln(value['name']) + ' - #' + key
-            })
             if (generateImages) {
                 pokemonIcon = `<img class='pokemon-select-icon' src='${getPokemonRawIconUrl({'pokemon_id': key})}'>`
             } else {
                 pokemonIcon = `<i class="pokemon-sprite n${key}"></i>`
             }
             value['name'] = i8ln(value['name'])
-            value['rarity'] = i8ln(value['rarity'])
             $.each(value['types'], function (key, pokemonType) {
-                _types.push({
-                    'type': i8ln(pokemonType['type']),
-                    'color': pokemonType['color']
-                })
                 typestring[key] = i8ln(pokemonType['type'])
             })
-            value['types'] = _types
             value['gen'] = getPokemonGen(key)
-            var gen = getPokemonGen(key)
-            $('.list').append('<div class=pokemon-icon-sprite data-gen=gen' + gen + ' data-pkm=' + i8ln(value['name']) + ' data-value=' + key + ' data-type1=' + typestring[0] + ' data-type2=' + typestring[1] + '><div id=pkid_list>#' + key + '</div>' + pokemonIcon + '<div id=pkname_list>' + i8ln(value['name']) + '</div></div>')
+            $('.list').append('<div class=pokemon-icon-sprite data-gen=gen' + value['gen'] + ' data-pkm=' + i8ln(value['name']) + ' data-value=' + key + ' data-type1=' + typestring[0] + ' data-type2=' + typestring[1] + '><div id=pkid_list>#' + key + '</div>' + pokemonIcon + '<div id=pkname_list>' + i8ln(value['name']) + '</div></div>')
             idToPokemon[key] = value
             pokeSearchList.push({
                 value: key,
                 pkm: i8ln(value['name']),
                 gen: 'gen' + value['gen'],
                 type1: typestring[0],
-                type2: typestring[1]
+                type2: typestring[1],
+                allpokemon: 'allpokemon'
             })
         })
 
@@ -3099,7 +3081,7 @@ $(function () {
             var id = img.data('key').toString()
             $('.hidepreset').removeClass('active')
             img.addClass('active')
-            generate_pokemon_exlude(hidepresets[id]["Searchstring"], hidepresets[id]["Invert"])
+            generate_pokemon_exlude(i8ln(hidepresets[id]["Searchstring"]), hidepresets[id]["Invert"])
         })
 
 
@@ -3116,16 +3098,7 @@ $(function () {
                 pokeselectlist.hide()
                 parent.parent().find('.select-all, .select-reverse').show()
                 parent.parent().find('.hide-all').hide()
-                var searchsplit = searchtext.split(' ')
-                $.each(searchsplit, function(k, searchstring) {
-                    if (searchstring !== '') {
-                        $.each(pokeSearchList, function (i, item) {
-                            if ((item["pkm"].toLowerCase().indexOf(searchstring.toLowerCase()) !== -1) || (item["type1"].toLowerCase().indexOf(searchstring.toLowerCase()) !== -1) || (item["gen"].toString() ===searchstring.toLowerCase())  || (item["value"].toString() === searchstring.toString())) {
-                                foundpokemon.push(item["value"])
-                            }
-                        })
-                    }
-                })
+                foundpokemon = filterpokemon(pokeSearchList , searchtext.replace(/\s/g,''))
             }
             $.each(foundpokemon, function(i, item) {
                 parent.next('.list').find('.pokemon-icon-sprite[data-value="' + foundpokemon[i]  + '"]').show()
@@ -3260,7 +3233,43 @@ $(function () {
 
     createUpdateWorker()
 
+    function isNumber(n) { return !isNaN(parseFloat(n)) && !isNaN(n - 0) }
 
+    function filterpokemon(pokemonarray, searchtext) {
+        if (searchtext.substring(0,1) === '-') { searchtext = 'allpokemon,' + searchtext }
+        var searchsplit = searchtext.split(',')
+        var foundpokemon = []
+        var operator = 'add'
+        $.each(searchsplit, function(k, searchstring) {
+            if (searchstring.substring(0,1) === '+') {
+                searchstring = searchstring.substring(1)
+                operator = 'add'
+            } else if (searchstring.substring(0,1) === '-') {
+                searchstring = searchstring.substring(1)
+                operator = 'remove'
+            } else {
+                operator = 'add'
+            }
+            if (isNumber(searchstring)) {
+                if (operator === 'add') {
+                    foundpokemon.push(searchstring)
+                } else {
+                    delete foundpokemon[foundpokemon.indexOf(searchstring)];
+                }
+             } else if (searchstring.length>0 &&  searchstring !== '-' && searchstring !== '+') {
+                 $.each(pokemonarray, function (i, item) {
+                     if ((item["pkm"].toLowerCase().indexOf(searchstring.toLowerCase()) !== -1) || (i8ln(item["type1"].toLowerCase()).indexOf(i8ln(searchstring).toLowerCase()) !== -1) || (i8ln(item["type2"].toLowerCase()).indexOf(i8ln(searchstring).toLowerCase()) !== -1) || (item["gen"].toString() === searchstring.toLowerCase())  || (item["value"].toString() === searchstring.toString()) || (item["allpokemon"].toString() === searchstring.toString()) ) {
+                         if (operator === 'add') {
+                             foundpokemon.push(item["value"])
+                         } else {
+                             delete foundpokemon[foundpokemon.indexOf(item["value"])];
+                         }
+                     }
+                 })
+             }
+        })
+        return foundpokemon
+    }
     // Wipe off/restore map icons when switches are toggled
     function buildSwitchChangeListener(data, dataType, storageKey) {
         return function () {
@@ -3326,20 +3335,28 @@ $(function () {
         }
     }
 
-    Array.prototype.diff = function (a) {
-        return this.filter(function (i) {
-        return a.indexOf(i) === -1;
-        });
+    function diffPokemon (array1, array2) {
+        var temp = [];
+        array1 = array1.toString().split(',').map(Number);
+        array2 = array2.toString().split(',').map(Number);
+        for (var i in array1) {
+            if(array2.indexOf(array1[i]) === -1) temp.push(array1[i]);
+        }
+        for(i in array2) {
+            if(array1.indexOf(array2[i]) === -1) temp.push(array2[i]);
+        }
+        return temp.sort((a,b) => a-b);
     }
 
 
     function generate_pokemon_exlude(value, invert) {
+        var value = value.toString()
         var allpokemon = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251,252,253,254,255,256,257,258,259,260,261,262,263,264,265,266,267,268,269,270,271,272,273,274,275,276,277,278,279,280,281,282,283,284,285,286,287,288,289,290,291,292,293,294,295,296,297,298,299,300,301,302,303,304,305,306,307,308,309,310,311,312,313,314,315,316,317,318,319,320,321,322,323,324,325,326,327,328,329,330,331,332,333,334,335,336,337,338,339,340,341,342,343,344,345,346,347,348,349,350,351,352,353,354,355,356,357,358,359,360,361,362,363,364,365,366,367,368,369,370,371,372,373,374,375,376,377,378,379,380,381,382,383,384,385,386];
         allpokemon = allpokemon.map(String)
         if (invert === false) {
-            $selectExclude.val(value).trigger('change');
+            $selectExclude.val(filterpokemon(pokeSearchList, value.replace(/\s/g, ''))).trigger('change');
         } else {
-            $selectExclude.val(allpokemon.diff(value.map(String))).trigger('change');
+            $selectExclude.val(diffPokemon(allpokemon, filterpokemon(pokeSearchList, value.replace(/\s/g, '')).map(String))).trigger('change');
         }
         loadDefaultImages()
         redrawPokemon(mapData.pokemons);
