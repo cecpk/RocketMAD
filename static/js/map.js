cad2d8678657f0507f34540a83a306ea75f5f60e
daangroot <groot.d@hotmail.nl>
2019-06-15 12:57:52 +0200

    Rename function names and pass gym to function.

--- static/js/map.js
+++ static/js/map.js
@@ -754,12 +754,12 @@ function isValidRaid(raid) {
     // && Date.now() > raid.spawn
 }
 
-function isGymSatisfiesRaidMinMaxFilter(raid) {
+function isRaidSatisfiesRaidMinMaxFilter(raid) {
     return raid && raid['level'] <= Store.get('showRaidMaxLevel') && raid['level'] >= Store.get('showRaidMinLevel')
 }
 
-function isGymSatisfiesRaidExEligibleFilter(raid) {
-    return raid && (!Store.get('showParkRaidsOnly') || raid.is_ex_raid_eligible)
+function isGymSatisfiesRaidExEligibleFilter(gym) {
+    return gym.raid && (!Store.get('showParkRaidsOnly') || gym.is_ex_raid_eligible)
 }
 
 function gymLabel(gym) {
@@ -819,7 +819,7 @@ function gymLabel(gym) {
             </div>`
     }
 
-    if ((isRaidUpcoming || isRaidOngoing) && isRaidFilterOn && isGymSatisfiesRaidMinMaxFilter(raid)) {
+    if ((isRaidUpcoming || isRaidOngoing) && isRaidFilterOn && isRaidSatisfiesRaidMinMaxFilter(raid)) {
         const raidColor = ['252,112,176', '255,158,22', '184,165,221']
         const levelStr = '★'.repeat(raid.level)
 
@@ -1367,13 +1367,13 @@ function updateGymMarker(gym, marker) {
     let markerImage = ''
     var zIndexOffset
 
-    if (gym.raid && isOngoingRaid(gym.raid) && Store.get('showRaids') && raidLevel >= Store.get('showRaidMinLevel') && raidLevel <= Store.get('showRaidMaxLevel') && isGymSatisfiesRaidExEligibleFilter(gym.raid)) {
+    if (gym.raid && isOngoingRaid(gym.raid) && Store.get('showRaids') && raidLevel >= Store.get('showRaidMinLevel') && raidLevel <= Store.get('showRaidMaxLevel') && isGymSatisfiesRaidExEligibleFilter(gym)) {
         markerImage = 'gym_img?team=' + gymTypes[gym.team_id] + '&level=' + getGymLevel(gym) + '&raidlevel=' + gym['raid']['level'] + '&pkm=' + gym['raid']['pokemon_id']
         if (gym.raid.form) {
             markerImage += '&form=' + gym.raid.form
         }
         zIndexOffset = 100
-    } else if (gym.raid && gym.raid.end > Date.now() && Store.get('showRaids') && !Store.get('showActiveRaidsOnly') && raidLevel >= Store.get('showRaidMinLevel') && raidLevel <= Store.get('showRaidMaxLevel') && isGymSatisfiesRaidExEligibleFilter(gym.raid)) {
+    } else if (gym.raid && gym.raid.end > Date.now() && Store.get('showRaids') && !Store.get('showActiveRaidsOnly') && raidLevel >= Store.get('showRaidMinLevel') && raidLevel <= Store.get('showRaidMaxLevel') && isGymSatisfiesRaidExEligibleFilter(gym)) {
         markerImage = 'gym_img?team=' + gymTypes[gym.team_id] + '&level=' + getGymLevel(gym) + '&raidlevel=' + gym['raid']['level']
         zIndexOffset = 20
     } else {
