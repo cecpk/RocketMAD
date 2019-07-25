666f88878d25f480f0843b93ed152554b5b17f09
daangroot <groot.d@hotmail.nl>
2019-07-25 18:05:40 +0200

    Change 'last lure' to 'last modified'.

--- static/js/map.js
+++ static/js/map.js
@@ -1125,7 +1125,7 @@ function pokestopLabel(pokestop) {
                   Last scanned: <span class='info'>${getDateStr(pokestop.last_updated)}</span>
                 </div>
                 <div>
-                  Last lure: <span class='info'>${getDateStr(pokestop.last_modified)}</span>
+                  Last modified: <span class='info'>${getDateStr(pokestop.last_modified)}</span>
                 </div>
                 <div>
                   <a href='javascript:void(0);' onclick='javascript:openMapDirections(${pokestop.latitude},${pokestop.longitude});' title='Open in ${mapLabel} Maps'>${pokestop.latitude.toFixed(7)}, ${pokestop.longitude.toFixed(7)}</a>
