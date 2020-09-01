bbcde9c8f79bfcfadf0c1772910fc648a4421b86
daangroot <groot.d@hotmail.nl>
2020-09-01 13:52:20 +0200

    Fix names and icons on quest page.

--- static/js/quest.js
+++ static/js/quest.js
@@ -146,6 +146,7 @@ $(function () {
                             case 7:
                                 rewardImageUrl = getPokemonRawIconUrl({pokemon_id: quest.pokemon_id, form: quest.form_id, costume: quest.costume_id})
                                 rewardText = `${getPokemonNameWithForm(quest.pokemon_id, quest.form_id)} <a href='https://pokemongo.gamepress.gg/pokemon/${quest.pokemon_id}' target='_blank' title='View on GamePress'>#${quest.pokemon_id}</a>`
+                                break
                             case 12:
                                 rewardImageUrl = getItemImageUrl(7)
                                 rewardText = getItemName(7)
