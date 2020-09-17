dbf5e816653d990d2560f10be7c14197455ab024
mickus99 <7754561+mickus99@users.noreply.github.com>
2020-09-18 00:23:00 +0930

    Update map.js
    test

--- static/js/map/map.js
+++ static/js/map/map.js
@@ -263,12 +263,15 @@ function initMap() { // eslint-disable-line no-unused-vars
         zoomControl: false,
         preferCanvas: true
     })
+    
+    console.log('custom_tileserver = ' + serverSettings.custom_tileserver)
 
     if (serverSettings.custom_tileserver != null || serverSettings.custom_tileserver != '') {
         Store.set('mapStyle', 'custom')
         setTileLayer(Store.get('mapStyle'), map)
     }
     else {
+        Store.set('mapStyle', 'Mapnik')
         setTileLayer(Store.get('mapStyle'), map)
     }
 
