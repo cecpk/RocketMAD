fbc0e9c013ece53cfe3fdbe7c8731a0798d4bea8
mickus99 <7754561+mickus99@users.noreply.github.com>
2020-09-17 22:02:00 +0930

    Update utils.leaflet.js

--- static/js/utils/utils.leaflet.js
+++ static/js/utils/utils.leaflet.js
@@ -6,7 +6,7 @@ const cartodbdarkmatternolabels = L.tileLayer('https://{s}.basemaps.cartocdn.com
 const cartodbpositron = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png', {maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'})
 const cartodbpositronnolabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}.png', {maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'})
 const satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {maxZoom: 20, attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'})
-const mapnik2 = L.tileLayer('https://tiles.downsouth.me/osm-bright/{z}/{x}/{y}.png', {maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'})
+const mapnik2 = L.tileLayer('https://tiles.downsouth.me/osm-bright/{z}/{x}/{y}/png', {maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'})
 
 function setTileLayer(layerName, map) {
     // Fallback in case layername does not exist (anymore).
