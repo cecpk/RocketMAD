9b131b768ef5a3d1e9f26064c7f39be7430fa3a5
Your Name <you@example.com>
2021-02-04 11:22:49 +0100

    Add invasion data script.

---
+++ scripts/generate_invasion_data.py
@@ -0,0 +1,39 @@
+from pogodata import PogoData
+import json
+
+
+if __name__ == "__main__":
+    data = PogoData()
+    result = {}
+
+    for grunt in data.grunts:
+        type_ = grunt.type.name
+        if not grunt.type:
+            type_ = ""
+
+        if grunt.name != "Grunt":
+            name = grunt.name
+        else:
+            name = "Male" if grunt.gender else "Female"
+        result[grunt.id] = {
+            "type": type_,
+            "grunt": name,
+        }
+        if grunt.active:
+            result[grunt.id]["pokemon"] = {
+                "1": {
+                    "ids": [m.id for m in grunt.team[0]],
+                    "isReward": True if 0 in grunt.reward_positions else False
+                },
+                "2": {
+                    "ids": [m.id for m in grunt.team[1]],
+                    "isReward": True if 1 in grunt.reward_positions else False
+                },
+                "3": {
+                    "ids": [m.id for m in grunt.team[2]],
+                    "isReward": True if 2 in grunt.reward_positions else False
+                },
+            }
+
+    with open("invasions.json", "w+") as f:
+        f.write(json.dumps(result, indent=2))
