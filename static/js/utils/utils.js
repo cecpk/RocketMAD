ed41ca2c0f10d9d4946af5e2f5bb34e4c57d6b95
wraythex <59678213+wraythex@users.noreply.github.com>
2021-09-03 19:02:54 -0500

    Change negTimeUntil to boolean

--- static/js/utils/utils.js
+++ static/js/utils/utils.js
@@ -198,9 +198,9 @@ function getTimeUntil(time) {
     var now = Date.now()
     var tdiff = time - now
 
-    var negTimeUntil = 0
+    var negTimeUntil = false
     if (tdiff < 0) {
-        negTimeUntil = 1
+        negTimeUntil = true
         tdiff *= -1
         tdiff += 1000
     }
@@ -209,7 +209,7 @@ function getTimeUntil(time) {
     var min = Math.floor((tdiff / 1000 / 60) % 60)
     var hour = Math.floor((tdiff / (1000 * 60 * 60)) % 24)
 
-    if (negTimeUntil === 1) {
+    if (negTimeUntil) {
       tdiff *= -1
       if (hour > 0) hour *= -1
       else if (min > 0) min *= -1
