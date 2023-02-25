8eda220d09c63ffa91ce046d2172122114748c41
Luiz Ganso <lzibeti@gmail.com>
2023-02-25 10:55:40 -0300

    Update Portuguese translation and Raid Level 10 check by default

--- static/js/utils/utils.store.js
+++ static/js/utils/utils.store.js
@@ -276,7 +276,7 @@ const StoreOptions = {
         type: StoreTypes.Boolean
     },
     includedRaidLevels: {
-        default: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        default: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
         type: StoreTypes.JSON
     },
     raidNotifs: {
