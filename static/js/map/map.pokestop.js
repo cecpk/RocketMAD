af9981d858ec974e591f0898efa97e87cd8fb85d
Thorben Otto <thorben_otto@gmx.de>
2023-09-07 09:23:35 +0200

    Update map.pokestop.js to show date and time

--- static/js/map/map.pokestop.js
+++ static/js/map/map.pokestop.js
@@ -342,7 +342,7 @@ function pokestopLabel(pokestop) {
                   </div>
                 </div>
                 <div class='disappear'>
-                  ${timestampToTime(invasionExpireTime)} (<span class='label-countdown' disappears-at='${invasionExpireTime}'>00m00s</span>)
+                  ${timestampToDateTime(invasionExpireTime)} (<span class='label-countdown' disappears-at='${invasionExpireTime}'>00m00s</span>)
                 </div>
                 <div class='info-container'>
                   ${typeDisplay}
